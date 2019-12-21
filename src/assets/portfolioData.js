9bcb9f0883e9271f489ac291a438ea84cc13056c
nahsiloh <lishan1512@gmail.com>
2019-12-21 11:57:06 +0800

    first commit

---
+++ src/assets/portfolioData.js
@@ -0,0 +1,26 @@
+import armsImage from "../assets/img/arms_app.jpg";
+import travelAppImage from "../assets/img/travel_app.jpg";
+import photoJournalImage from "../assets/img/photojournal_app.jpg";
+
+const portfolioData = [
+  {
+    title: "Lets fly!",
+    description: "Plan you next travel itinerary",
+    image: travelAppImage,
+    url: "https://letsfly.netlify.com"
+  },
+  {
+    title: "Apartments and residential management system",
+    description: "Staff accommodation tracker",
+    image: armsImage,
+    url: "https://demo-armadillo-arms.herokuapp.com"
+  },
+  {
+    title: "My Insta",
+    description: "Photo journal",
+    image: photoJournalImage,
+    url: "https://myinstajournal.herokuapp.com"
+  }
+];
+
+export default portfolioData;
