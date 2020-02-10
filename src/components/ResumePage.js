0a9115ebf5eea0bce3b571093591f0d6a9e1ff5f
nahsiloh <lishan1512@gmail.com>
2020-02-10 11:41:23 +0800

    edited resume

--- src/components/ResumePage.js
+++ src/components/ResumePage.js
@@ -7,7 +7,7 @@ class ResumePage extends React.Component {
       <div className="resumePage__Main">
         <section className="resumePage__Link">
           <p className="resumePage__paragraph">Download Resume</p>
-          <a href="https://drive.google.com/file/d/1DMvAirwkCrkfqxKha4VVEKQxcxf8pE2x/view?usp=sharing">
+          <a href="https://drive.google.com/file/d/1IMdPjJM4IuzIvi2WBAl0sB9rzeOtbdmX/view?usp=sharing">
             <i class="fas fa-file-download"></i>
           </a>
         </section>
